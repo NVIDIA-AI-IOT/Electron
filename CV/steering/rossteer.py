3b3e0b2e9263e1eb78ffa25d3343e07c86498528
NVIDIA <mvoodarla@gmail.com>
2017-07-11 15:26:59 -0700

    updated rossteer

--- CV/steering/rossteer.py
+++ CV/steering/rossteer.py
@@ -1,4 +1,5 @@
 import keras
+import rospy
 from keras.layers import Input, Dense, merge
 from keras.models import Model
 from keras.models import Sequential
@@ -8,22 +9,31 @@ from keras.regularizers import l2
 from keras.utils import plot_model
 from keras.models import load_model
 
-scanDataText = ''
-model = load_model('actconv2d.h5')
-stxt = scanDataText.split('\n')
-lidarData = []
-for line in stxt:
-    line = line.replace('inf', 'None') # replace string so that I can use eval function
-    if line.startswith('ranges'):
-        edited = line[8:] # cuts the string so that it is left only with an array
-        lidarData = eval(edited)
-        break
+def callback(data):
+    scanDataText = ''
+    model = load_model('actconv2d.h5')
+    stxt = scanDataText.split('\n')
+    lidarData = []
+    for line in stxt:
+        line = line.replace('inf', 'None') # replace string so that I can use eval function
+        if line.startswith('ranges'):
+            edited = line[8:] # cuts the string so that it is left only with an array
+            lidarData = eval(edited)
+            break
 
-count1 = 0
-for y in lidarData:
-    lidarData[count1] = [count1+1, y]
-    count1 = count1 + 1
+    count1 = 0
+    for y in lidarData:
+        lidarData[count1] = [count1+1, y]
+        count1 = count1 + 1
 
-o = model.predict(np.array([resize]), batch_size=32, verbose=2)
-output = o[0]
-jstk = output[0]
+    o = model.predict(np.array(lidarData), batch_size=32, verbose=2)
+    output = o[0]
+    jstk = output[0]
+
+def listener():
+    rospy.init_node('steerNode', anonymous=True)
+    rospy.Subscriber("scan", String, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
