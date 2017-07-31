d5509d5b35c330406b5cd6230d7f840b5f4e80f0
Nicholas Liu <nicholas.j.liu@gmail.com>
2017-07-31 15:11:48 -0700

    Fixed Merge Conflict

--- Turtlebot2/tb2_2dnav/scripts/ImageNetDrive.py
+++ Turtlebot2/tb2_2dnav/scripts/ImageNetDrive.py
@@ -14,11 +14,7 @@ class Synchronize:
     def callback_item(self, data):
     	print('item callback')
 	self.item = str(data)
-<<<<<<< HEAD:Turtlebot2/tb2_2dnav/scripts/ImageNet2Drive.py
-    	if "0" in str(data):
-=======
     	if "0" in data and self.state is "deliver":
->>>>>>> 3a900a9b13103ddd7107a955d98ea847013d9267:Turtlebot2/tb2_2dnav/scripts/ImageNetDrive.py
         	print('set to 0')
         	self.twist.linear.x = 0; self.twist.linear.y = 0; self.twist.linear.z = 0
         	self.twist.angular.x = 0; self.twist.angular.y = 0; self.twist.angular.z = 0
@@ -38,21 +34,13 @@ class Synchronize:
 
 	rospy.Subscriber('/cmd_vel', Twist, self.callback_twist)
     	rospy.Subscriber('/item', String, self.callback_item)
-<<<<<<< HEAD:Turtlebot2/tb2_2dnav/scripts/ImageNet2Drive.py
-	
-=======
 	rospy.Subscriber('/dstate', String, self.callback_item)
 
->>>>>>> 3a900a9b13103ddd7107a955d98ea847013d9267:Turtlebot2/tb2_2dnav/scripts/ImageNetDrive.py
 	pub = rospy.Publisher("/cmd_vel_mux/input/teleop", Twist, queue_size=5)
 	
 	while not rospy.is_shutdown():
 		if self.send_item:
 			pub.publish(self.twist)
-<<<<<<< HEAD:Turtlebot2/tb2_2dnav/scripts/ImageNet2Drive.py
-=======
-			self.send_twist = False
->>>>>>> 3a900a9b13103ddd7107a955d98ea847013d9267:Turtlebot2/tb2_2dnav/scripts/ImageNetDrive.py
 			self.send_item = False
 
 if __name__ == '__main__':
